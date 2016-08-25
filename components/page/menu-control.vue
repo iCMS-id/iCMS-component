d6bdd8814aab6e0e98e089e3413fceab7d86895d
nothing628 <yogyphang@gmail.com>
2016-08-26 03:25:39 +0700

    add delete url to menu control

--- components/page/menu-control.vue
+++ components/page/menu-control.vue
@@ -8,6 +8,11 @@
 				type: String,
 				default: null,
 				required: true
+			},
+			deleteUrl: {
+				type: String,
+				default: null,
+				required: true
 			}
 		},
 		methods: {
@@ -36,9 +41,26 @@
 					}
 				});
 			},
+			deleteMenu: function (evt, elm) {
+				var id = elm.attr('data-id');
+				var that = this;
+
+				$.ajax({
+					url: that.deleteUrl,
+					data: {id: id},
+					dataType: 'json',
+					method: 'post',
+					success: function (data) {
+						if (data.success) {
+							that.$dispatch('data-should-update');
+						}
+					}
+				});
+			},
 			detachEvent: function () {
 				$('body').off('click', 'a.menu.up:not(.disabled)');
 				$('body').off('click', 'a.menu.down:not(.disabled)');
+				$('bodu').off('click', 'a.menu.delete');
 			},
 			attachEvent: function () {
 				var that = this;
@@ -49,6 +71,10 @@
 
 				$('body').on('click', 'a.menu.down:not(.disabled)', function (evt) {
 					that.downMenu(evt, $(this));
+				});
+
+				$('body').on('click', 'a.menu.delete', function (evt) {
+					that.deleteMenu(evt, $(this));
 				});
 			}
 		},
